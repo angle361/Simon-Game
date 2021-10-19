bbe8291ecca96a0757c950370c632ec21034670b
varunsahu29.vs <varunsahu29.vs@gmail.com>
2021-10-19 19:00:24 +0530

    invoked return key
    Signed-off-by: varunsahu29.vs <varunsahu29.vs@gmail.com>

--- game.js
+++ game.js
@@ -1,89 +1,89 @@
 //alert("working");
 //$("h1").text("hello");
-var buttonColours=["red","blue","green","yellow"] ;
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
-var level= 0;
+var level = 0;
 
 // alert("Yes");
 // For adding functionality to game
 
-$(document).keypress(function() {
-    if(!started)
-    {
-        $("#level-title").text("Level "+level);
-        nextSequence();
-        started=true;
-    }    
+$(document).keypress(function (e) {
+  if (e.key == "Enter" && !started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 });
 
-$(".btn").click( function() {
-var userChosenColour= $(this).attr("id");
-userClickedPattern.push(userChosenColour);
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
 
-playSound(userChosenColour);
-animatePress(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 
-checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 /*8*/
-function checkAnswer(currentLevel)
-{
-    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
-        //console.log("success");
-        if (userClickedPattern.length === gamePattern.length){
-            setTimeout(function () {
-                nextSequence();
-              }, 1000);
-        }
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    //console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
     }
-/*9*/    else{
-        //console.log("failure");
-        playSound("wrong");
+  } else {
+    /*9*/
+    //console.log("failure");
+    playSound("wrong");
 
-        $("body").addClass("game-over");
-        $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
 
-        setTimeout(function () {
-            $("body").removeClass("game-over");
-        }, 200);
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
 
-        startOver();
-    }
+    startOver();
+  }
 }
 
 /*1*/
-function nextSequence(){
-    userClickedPattern = []; //i have done big mistake here
-    level++;
-    $("#level-title").text("Level "+level);
-    var rn=Math.floor(Math.random() * 4);
-    
-    var randomChosenColour= buttonColours[rn];
-    
-    gamePattern.push(randomChosenColour);
+function nextSequence() {
+  userClickedPattern = []; //i have done big mistake here
+  level++;
+  $("#level-title").text("Level " + level);
+  var rn = Math.floor(Math.random() * 4);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColour);
-    
+  var randomChosenColour = buttonColours[rn];
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
 }
-function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
-s
-function animatePress(currentColour){
-    $("#"+currentColour).addClass("pressed");
-    setTimeout(function(){
-    $("#"+currentColour).removeClass("pressed");
-    }, 100);
+s;
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
 
 /*10*/
-function startOver(){
-    level = 0;
-    gamePattern = [];
-    started = false;
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
